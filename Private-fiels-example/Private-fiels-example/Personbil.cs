bc7b4229ad96dee4981cc3822304df1a93fc6977
Alexander Møller <leroikier@hotmail.com>
2018-08-27 15:22:35 +0200

    <Eksempel på Private fields med try catch metodee>

---
+++ Private-fiels-example/Private-fiels-example/Personbil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_fiels_example
+{
+    public class Personbil
+    {
+        private string Model;
+        private string Mærke;
+        private double KmPerLiter;
+        private int Årgang;
+
+
+        public void Initialiser(string model, string mærke,
+    double kmPerLiter, int årgang)
+        {
+            // kopiering af værdier fra argumenter til interne felter
+            this.Model = model;
+            this.Mærke = mærke;
+            this.KmPerLiter = kmPerLiter;
+            // eksempel på validering
+            if (årgang < DateTime.Now.Year - 100 ||
+                årgang > DateTime.Now.Year + 1)
+                throw new ArgumentException("Forkert årgang");
+            else
+                this.Årgang = årgang;
+        }
+    }
+}
---
+++ Private-fiels-example/Private-fiels-example/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_fiels_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eksempel på en bil som ikke bliver oprettet
+            try
+            {
+                //opretter en ny Personbil
+                Personbil b1 = new Personbil();
+                //Sætter værdier
+                b1.Initialiser("Peugeot", "406", 13.4, 1900);
+                //dette udskrvies hvis bilen er under 100 år gammel
+                Console.WriteLine("Peugeot Bil oprettet");
+            }
+            //Hvis bilen er over 100 år gammel udskrvies denne exception
+            catch (Exception ex)
+            {
+                Console.WriteLine("Der er desværre sket en fejl - " + ex.Message);
+            }
+
+
+            //Eksempel på en bil som bliver oprettet
+            try
+            {
+                Personbil b2 = new Personbil();
+                b2.Initialiser("BMW", "M5", 17.4, 2001);
+                Console.WriteLine("BMW Bil oprettet");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Der er desværre sket en fejl - " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
